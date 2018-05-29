2898fe45c652bb633e056b2ede85a515eb509d62
Natasha Cox <natashac1014@gmail.com>
2018-05-29 11:53:22 -0500

    pushing up

---
+++ main.js
@@ -0,0 +1,48 @@
+const ElizabetheSanger = {
+    congressionalDistrict: "District 5",
+    platformStatements: {
+            taxes: "I will lower tax rates",
+            jobs: "I will create more jobs",
+            Infrastructure: "Build infrastructure",
+            HealthCare: "Make healthcare more affordable",
+            crimeAndEnforcement: "Enact a no tolerance policy"
+        }
+}
+const donation = "https://secure.actblue.com/donate/gillibrand_homepage";
+
+const calendar = [
+    {
+        date: "May 29",
+        event: "Meet and Greet"
+    },
+    {   date: "May 30",
+        event: "Hang door fliers"
+    },
+    {
+        date: "June 1",
+        event: "Fundraiser"
+    }
+]
+
+const volunteerInformation = {
+    fullName: "Natasha Cox",
+    address: "708 Montgomery St",
+    email: "natashac@gmail.cocm",
+    phoneNumber: "555-555-5555",
+    availability: "Saturday and Sunday",
+    activities: "event setup, fliers, accounting",
+}
+
+const imageGallery = {
+    headShot: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fd%2Fdf%2FKirsten_Gillibrand%252C_official_portrait%252C_112th_Congress.jpg%2F1200px-Kirsten_Gillibrand%252C_official_portrait%252C_112th_Congress.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FKirsten_Gillibrand&docid=tLQiQBxHVPPfQM&tbnid=cExxXHUcVgkjBM%3A&vet=10ahUKEwjd2Zi1oKvbAhVuuVkKHQR9Ci0QMwg1KAEwAQ..i&w=1200&h=1520&bih=740&biw=756&q=Kirsten%20Gillibrand&ved=0ahUKEwjd2Zi1oKvbAhVuuVkKHQR9Ci0QMwg1KAEwAQ&iact=mrc&uact=8',
+    familyPhoto: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fc%2Fc4%2FKirsten_Gillibrand_and_Family_-_Halloween_2009.jpg%2F220px-Kirsten_Gillibrand_and_Family_-_Halloween_2009.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FKirsten_Gillibrand&docid=tLQiQBxHVPPfQM&tbnid=KzjuVOnY7x7CqM%3A&vet=10ahUKEwjam6bZoKvbAhUNwlkKHRVqB_YQMwg3KAAwAA..i&w=220&h=199&bih=740&biw=756&q=Kirsten%20Gillibrand%20family&ved=0ahUKEwjam6bZoKvbAhUNwlkKHRVqB_YQMwg3KAAwAA&iact=mrc&uact=8',
+    constituentPhoto: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi2.wp.com%2Fwww.kingscountypolitics.com%2Fwp-content%2Fuploads%2F2017%2F09%2FIMG_1400.jpg%3Fresize%3D500%252C375%26ssl%3D1&imgrefurl=https%3A%2F%2Fwww.kingscountypolitics.com%2Fgillibrand-medgar-evers-town-hall-addresses-daca-titile-ix-climate-change-education%2F&docid=2yMd4UT-vCY_GM&tbnid=2aXD5c6AeCat5M%3A&vet=10ahUKEwj5kP3xoKvbAhUuuVkKHXkkBqQQMwg0KAIwAg..i&w=500&h=375&bih=740&biw=756&q=kirsten%20gillibrand%20constituents&ved=0ahUKEwj5kP3xoKvbAhUuuVkKHXkkBqQQMwg0KAIwAg&iact=mrc&uact=8',
+}
+
+const missionStatement = "Through hard work, bold action, and a focus on helping others, Elizabeth is a public servant dedicated to bringing people together to create change."
+
+const registerToVote = 'https://sos.tn.gov/products/elections/register-vote';
+
+function changePlatformsStatements (ElizabetheSanger.platformStatements, propertyToChange) {
+    console.log(changePlatforms)
+}
